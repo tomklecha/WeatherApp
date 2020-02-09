ee9dd6fdcd29b600cc448021696a6a3d67619cc0
tomklecha <tom.klecha.dev@gmail.com>
2020-02-09 10:44:17 +0000

    weather dates displayed properly

--- app/src/main/java/com/tkdev/weatherapp/presenter/CurrentPresenter.java
+++ app/src/main/java/com/tkdev/weatherapp/presenter/CurrentPresenter.java
@@ -2,11 +2,13 @@ package com.tkdev.weatherapp.presenter;
 
 import android.util.Log;
 import android.widget.TextView;
+
 import com.tkdev.weatherapp.model.Weather;
 import com.tkdev.weatherapp.modelretro.WeatherRetrofit;
 import com.tkdev.weatherapp.repository.WeatherRetrofitImpl;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import retrofit2.Response;
 
@@ -23,7 +25,7 @@ public class CurrentPresenter implements MainContract.Presenter, MainContract.AP
         this.view = view;
         this.weather = new WeatherRetrofit();
         this.model = new WeatherRetrofitImpl();
-        }
+    }
 
     @Override
     public void onWeatherCreated() {
@@ -35,39 +37,6 @@ public class CurrentPresenter implements MainContract.Presenter, MainContract.AP
         this.view = null;
     }
 
-    // TestView text changers
-
-
-    public String setTemperatureCurrentTextView() {
-       return (Math.rint((weather.getMain().getTemp()) * 10) / 10) + Weather.TEMPERATURE_SUFFIX;
-    }
-
-    public String  setTemperatureMinimumTextView() {
-        return (Math.rint((weather.getMain().getTempMin()) * 10) / 10)+ Weather.TEMPERATURE_SUFFIX;
-
-    }
-
-    public String setTemperatureMaximumTextView() {
-        return (Math.rint((weather.getMain().getTempMax()) * 10) / 10) + Weather.TEMPERATURE_SUFFIX;
-    }
-
-    public String setWeatherDescriptionTextView() {
-       return weather.getWeather().get(0).getMain();
-    }
-
-    public String setHumidityViewText() {
-       return  (weather.getMain().getHumidity()) + Weather.HUMIDITY_SUFFIX;
-    }
-
-    public String setLastUpdateViewText() {
-        SimpleDateFormat lastUpdate = new SimpleDateFormat(LAST_UPDATE_PATTERN);
-        return lastUpdate.format(weather.getDt()*1000);
-    }
-    public String setDateViewText() {
-        SimpleDateFormat lastUpdate = new SimpleDateFormat(DATE_PATTERN);
-       return lastUpdate.format(weather.getDt()*1000);
-    }
-
     @Override
     public void onSuccess(Response<WeatherRetrofit> response) {
         weather = response.body();
@@ -76,5 +45,41 @@ public class CurrentPresenter implements MainContract.Presenter, MainContract.AP
     @Override
     public void onFailure(Throwable t) {
 
+    }
+
+    // TestView text changers
+
+
+    public String setTemperatureCurrentTextView() {
+        return (Math.rint((weather.getMain().getTemp()) * 10) / 10) + Weather.TEMPERATURE_SUFFIX;
+    }
+
+    public String setTemperatureMinimumTextView() {
+        return (Math.rint((weather.getMain().getTempMin()) * 10) / 10) + Weather.TEMPERATURE_SUFFIX;
+
+    }
+
+    public String setTemperatureMaximumTextView() {
+        return (Math.rint((weather.getMain().getTempMax()) * 10) / 10) + Weather.TEMPERATURE_SUFFIX;
+    }
+
+    public String setWeatherDescriptionTextView() {
+        return weather.getWeather().get(0).getMain();
+    }
+
+    public String setHumidityViewText() {
+        return (weather.getMain().getHumidity()) + Weather.HUMIDITY_SUFFIX;
+    }
+
+    public String setLastUpdateViewText() {
+        SimpleDateFormat lastUpdate = new SimpleDateFormat(LAST_UPDATE_PATTERN);
+        Long date = Long.valueOf(weather.getDt());
+        return lastUpdate.format(date * 1000);
+    }
+
+    public String setDateViewText() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_PATTERN);
+        Long date = Long.valueOf(weather.getDt());
+        return dateFormat.format(date * 1000);
     }
 }
